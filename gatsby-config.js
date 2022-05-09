47a7e9c2449ce680e20f380698cc4c6ddd70d721
gulcinuras <gulcinuras@sabanciuniv.edu>
2022-05-09 18:19:58 +0300

    feat(gasby-config): Add ga and gtm plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-      title: `Tinyman`,
-    siteUrl: `https://tinyman.org/`
+    title: `Tinyman`,
+    siteUrl: `https://tinyman.org/`,
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -23,12 +23,34 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-google-tagmanager",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        id: "GTM-MZMGQ9K",
-        includeInDevelopment: true,
-        defaultDataLayer: { platform: "gatsby" }
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-215302739-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
       },
     },
-  ]
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-P8K9H2C",
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
+  ],
 };
